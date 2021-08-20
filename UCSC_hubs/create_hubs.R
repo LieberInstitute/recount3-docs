e1d089a8fc5bdf6b8695a9bc7f68e5fb27d56baf
Leonardo Collado Torres <lcolladotor@gmail.com>
2021-08-20 16:46:04 -0400

    use the file source in the project dir

--- UCSC_hubs/create_hubs.R
+++ UCSC_hubs/create_hubs.R
@@ -119,14 +119,20 @@ create_hub(
     x,
     hub_name = "DRP001299",
     email = "lcolladotor@gmail.com",
-    output_dir = here("UCSC_hubs", "mouse", "DRP001299")
+    output_dir = here("UCSC_hubs", "mouse", "sra_DRP001299")
 )
 
 ## test with
-## https://genome.ucsc.edu/cgi-bin/hgTracks?db=mm10&lastVirtModeType=default&lastVirtModeExtraState=&virtModeType=default&virtMode=0&nonVirtPosition=&position=chr1&hubUrl=https://raw.githubusercontent.com/LieberInstitute/recount3-docs/master/UCSC_hubs/mouse/DRP001299/hub.txt
+## https://genome.ucsc.edu/cgi-bin/hgTracks?db=mm10&lastVirtModeType=default&lastVirtModeExtraState=&virtModeType=default&virtMode=0&nonVirtPosition=&position=chr1&hubUrl=https://raw.githubusercontent.com/LieberInstitute/recount3-docs/master/UCSC_hubs/mouse/sra_DRP001299/hub.txt
 
 ## can also work for one sample at a time like at
 ## http://genome.ucsc.edu/cgi-bin/hgTracks?db=mm10&position=chr1&hgct_customText=track%20type=bigWig%20name=DRR014696%20description=%22a%20bigWig%20track%22%20visibility=full%20bigDataUrl=http://duffel.rail.bio/recount3/mouse/data_sources/sra/base_sums/99/DRP001299/96/sra.base_sums.DRP001299_DRR014696.ALL.bw
+
+## Create for all projects
+samples_by_proj <- split(samples, paste0(samples$project, "-", samples$organism))
+xx <- lapply(samples_by_proj[1:3], function(x) {
+    create_hub(x, hub_name = x$project[1], email = "lcolladotor@gmail.com", output_dir = here("UCSC_hubs", x$organism[1], paste0(x$file_source[1], "_", x$project[1])))
+})
 
 ## Reproducibility information
 print('Reproducibility information:')
