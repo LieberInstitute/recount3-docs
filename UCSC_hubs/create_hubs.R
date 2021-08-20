ba8806651c26ec48ed70fe6a6aafeaca76f30f9a
Leonardo Collado Torres <lcolladotor@gmail.com>
2021-08-20 15:30:49 -0400

    Auto ident code

--- UCSC_hubs/create_hubs.R
+++ UCSC_hubs/create_hubs.R
@@ -12,36 +12,78 @@ create_hub <-
         hub_long_label = "Wilks et al, 2021. recount3: summaries and queries for large-scaleRNA-seq expression and splicing.",
         hub_description_url = "http://rna.recount.bio/",
         recount3_url = getOption("recount3_url", "http://duffel.rail.bio/recount3")) {
-
         stopifnot(is(x, "data.frame"))
-        stopifnot(all(c("external_id", "project", "organism", "file_source", "project_home") %in% colnames(x)))
+        stopifnot(all(
+            c(
+                "external_id",
+                "project",
+                "organism",
+                "file_source",
+                "project_home"
+            ) %in% colnames(x)
+        ))
         stopifnot(length(unique(x$organism)) == 1)
 
         dir.create(output_dir, recursive = TRUE)
 
-        content_hub <- paste0("hub ", hub_name, "\nshortLabel ", hub_short_label, "\nlongLabel ", hub_long_label, "\ngenomesFile genomes.txt\nemail ", email, "\ndescriptionUrl ", hub_description_url, "\n")
+        content_hub <-
+            paste0(
+                "hub ",
+                hub_name,
+                "\nshortLabel ",
+                hub_short_label,
+                "\nlongLabel ",
+                hub_long_label,
+                "\ngenomesFile genomes.txt\nemail ",
+                email,
+                "\ndescriptionUrl ",
+                hub_description_url,
+                "\n"
+            )
         writeLines(content_hub, file.path(output_dir, "hub.txt"))
 
         org <- ifelse(unique(x$organism) == "human", "hg38", "mm10")
         dir.create(file.path(output_dir, org))
-        content_genomes <- paste0("genome ", org, "\n trackDb ", org, "/trackDb.txt")
+        content_genomes <-
+            paste0("genome ", org, "\ntrackDb ", org, "/trackDb.txt")
         writeLines(content_genomes, file.path(output_dir, "genomes.txt"))
 
         by_proj <- split(x, paste0(x$project, "-", x$organism))
 
         x_bw <- do.call(rbind, lapply(by_proj, function(proj) {
-            cbind(proj, bigWig = locate_url(
-                project = proj$project[1],
-                project_home = proj$project_home[1],
-                type = "bw",
-                organism = proj$organism[1],
-                recount3_url = recount3_url,
-                sample = x$external_id
-            ))
+            cbind(
+                proj,
+                bigWig = locate_url(
+                    project = proj$project[1],
+                    project_home = proj$project_home[1],
+                    type = "bw",
+                    organism = proj$organism[1],
+                    recount3_url = recount3_url,
+                    sample = x$external_id
+                )
+            )
         }))
 
-        content_trackDb <- paste0("track ", x_bw$external_id, "\nbigDataUrl ", x_bw$bigWig, "\nshortLabel recount3 coverage bigWig for sample ", x_bw$external_id, "\nlongLabel recount3 bigWig for external id: ", x_bw$external_id, ", project: ", x_bw$project, ", file_source: ", x_bw$file_source, ", organism: ", x_bw$organism, "\ntype bigWig\n")
-        writeLines(content_trackDb, file.path(output_dir, org, "trackDb.txt"))
+        content_trackDb <-
+            paste0(
+                "track ",
+                x_bw$external_id,
+                "\nbigDataUrl ",
+                x_bw$bigWig,
+                "\nshortLabel recount3 coverage bigWig for sample ",
+                x_bw$external_id,
+                "\nlongLabel recount3 bigWig for external id: ",
+                x_bw$external_id,
+                ", project: ",
+                x_bw$project,
+                ", file_source: ",
+                x_bw$file_source,
+                ", organism: ",
+                x_bw$organism,
+                "\ntype bigWig\n"
+            )
+        writeLines(content_trackDb,
+            file.path(output_dir, org, "trackDb.txt"))
 
     }
 
@@ -53,7 +95,12 @@ samples <- rbind(recount3::available_samples("human"),
 
 ## Test with DRP001299
 x <- subset(samples, project == "DRP001299")
-create_hub(x, hub_name = "DRP001299", email = "lcolladotor@gmail.com", output_dir = here("UCSC_hubs", "mouse", "DRP001299"))
+create_hub(
+    x,
+    hub_name = "DRP001299",
+    email = "lcolladotor@gmail.com",
+    output_dir = here("UCSC_hubs", "mouse", "DRP001299")
+)
 
 ## test with https://genome.ucsc.edu/cgi-bin/hgTracks?db=mm10&lastVirtModeType=default&lastVirtModeExtraState=&virtModeType=default&virtMode=0&nonVirtPosition=&position=chr1:1-10000&hubUrl=https://raw.githubusercontent.com/LieberInstitute/recount3-docs/master/UCSC_hubs/mouse/DRP001299/hub.txt&hgt.customText=http://rna.recount.bio/
 
